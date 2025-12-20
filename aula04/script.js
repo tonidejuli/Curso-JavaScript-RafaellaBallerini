c2228c37d86e3a4d434c54527ff597ad1cb24869
tonidejuli <220539207+tonidejuli@users.noreply.github.com>
2025-12-20 13:23:12 -0300

    array

---
+++ aula04/script.js
@@ -0,0 +1,36 @@
+let tarefas = []
+
+function adicionarTarefa() {
+
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+
+    const mensagem = document.getElementById("mensagem")
+
+
+    if (tarefa == "") {
+        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
+        mensagem.textContent = mensagemErro
+    } else {
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+        tarefas.push(tarefa)
+        renderizarTarefas()
+
+    }
+
+    inputTarefa.value = ""
+}
+
+function renderizarTarefas() {
+    const listaTarefas = document.getElementById("listaTarefas")
+    listaTarefas.innerHTML = ""
+    let i = 0
+    for (i; i < tarefas.length; i++) {
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefas[i]
+        listaTarefas.appendChild(novaTarefa)
+    }
+
+    
+}
