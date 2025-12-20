6a3cb417fcd2185bfcb3e60e4ecc7222aba59f4f
tonidejuli <220539207+tonidejuli@users.noreply.github.com>
2025-12-20 11:51:20 -0300

    ajustes

--- aula02/script.js
+++ aula02/script.js
@@ -1,18 +1,30 @@
 function adicionarTarefa() {
-    let mensagem = "Tarefa adicionada com sucesso!";
-    
-
     let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
-    document.getElementById("mensagem").textContent = mensagem;
+    let mensagem = document.getElementById("mensagem")
 
-    let listaTarefas = document.getElementById("listaTarefas")
-    let novaTarefa = document.createElement("li")
-
-    novaTarefa.textContent = tarefa
-
-    listaTarefas.appendChild(novaTarefa)
+    if(inputTarefa.value.length == 0){
+        mensagem.innerText = "Não foi possível adicionar uma tarefa, é necessário digitar alguma."
+    } else {
+        let mensagem = "Tarefa adicionada com sucesso!";
     
 
-    inputTarefa.value = ""
+    
+        let tarefa = inputTarefa.value
+        document.getElementById("mensagem").textContent = mensagem;
+
+        let listaTarefas = document.getElementById("listaTarefas")
+        let novaTarefa = document.createElement("li")
+
+        novaTarefa.textContent = tarefa
+
+        listaTarefas.appendChild(novaTarefa)
+    
+
+        inputTarefa.value = ""
+    }
+}
+
+function Limpar(){
+    mensagem.innerHTML = ""
+    listaTarefas.innerHTML = ""
 }
