7c6515ee4536e2409e78ec683b1878e6d88aff20
tonidejuli <220539207+tonidejuli@users.noreply.github.com>
2025-12-22 14:09:48 -0300

    criando limpar

--- aula04/script.js
+++ aula04/script.js
@@ -25,12 +25,31 @@ function adicionarTarefa() {
 function renderizarTarefas() {
     const listaTarefas = document.getElementById("listaTarefas")
     listaTarefas.innerHTML = ""
-    let i = 0
-    for (i; i < tarefas.length; i++) {
+    
+    for (let i = 0; i < tarefas.length; i++) {
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefas[i]
+        
+        let botaoRemover = document.createElement("button")
+        botaoRemover.className = "remover"
+        botaoRemover.textContent = "Remover"
+        botaoRemover.onclick = () => removerTarefa(i)
+
+        let botaoEditar = document.createElement("button")
+        botaoEditar.className = "editar"
+        botaoEditar.textContent = "Editar"
+        botaoEditar.onclick = () => editarTarefa(i)
+        novaTarefa.appendChild(botaoRemover)
+        novaTarefa.appendChild(botaoEditar)
         listaTarefas.appendChild(novaTarefa)
     }
+}
 
+function removerTarefa(i) {
+    tarefas.splice(i, 1)
+    renderizarTarefas()
+}
+
+function editarTarefa(i){
     
 }
